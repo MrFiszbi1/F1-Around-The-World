c3e5a14854ec9773a66621201f87ce7c329c8f5b
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-05-16 11:28:52 -0700

    hello
    hello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
+    <div className='App'>
+      <h1>Hello From F1 Around The World</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
