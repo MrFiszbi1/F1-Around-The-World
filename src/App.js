242cd1d8d929793c9e4f2013f6914112a582b69a
Gabriel Sampang <gsampang@cecs.pdx.edu>
2023-05-31 16:18:40 -0700

    Successfully fetched data from API and data files, needs refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './styles.css';
+import "./styles.css";
 
 export default function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Hello From F1 Around The World</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
@@ -12,7 +12,12 @@ export default function App() {
 // urls for the ergast API, fetching up to 1000 data points
 const races_url = "http://ergast.com/api/f1.json?limit=1000";
 const drivers_url = "http://ergast.com/api/f1/drivers.json?limit=1000";
-const constructors_url = "http://ergast.com/api/f1/constructors.json?limit=300"
+const constructors_url = "http://ergast.com/api/f1/constructors.json?limit=300";
+
+// downloaded files linked
+const races_file = "./data/races.json";
+const drivers_file = "./data/drivers.json";
+const constructors_file = "./data/constructors.json";
 
 const fetchData = async (url) => {
   try {
@@ -26,10 +31,17 @@ const fetchData = async (url) => {
   }
 };
 
-fetchData(races_url);           //races data, limit 1000. Working
-fetchData(drivers_url);         //driver data, limit 1000. Working
-fetchData(constructors_url);    //driver data, limit 300. Working
+//fetching from the API
+fetchData(races_url); //races data, limit 1000. Working
+fetchData(drivers_url); //driver data, limit 1000. Working
+fetchData(constructors_url); //driver data, limit 300. Working
 
+//fetching from the downloaded files
+fetchData(races_file);
+fetchData(drivers_file);
+fetchData(constructors_file);
+
+//Used for downloading the data
 //print data as a string in the console, copy and place in data folder
 const printData = async (url) => {
   try {
@@ -44,7 +56,7 @@ const printData = async (url) => {
   }
 };
 
-console.log("Printing Key Data:");
-printData(races_url);           //races data, limit 1000. Working
-printData(drivers_url);         //driver data, limit 1000. Working
-printData(constructors_url);    //driver data, limit 300. Working
+// console.log("Printing Key Data:");
+// printData(races_url); //races data, limit 1000. Working
+// printData(drivers_url); //driver data, limit 1000. Working
+// printData(constructors_url); //driver data, limit 300. Working
