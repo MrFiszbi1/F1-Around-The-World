a6c8769b2be0738f10644a0f4bc92d9d602abdd5
Gabriel Sampang <gsampang@pdx.edu>
2023-05-30 18:47:39 -0700

    Added key data files to data folder

--- src/App.js
+++ src/App.js
@@ -29,3 +29,22 @@ const fetchData = async (url) => {
 fetchData(races_url);           //races data, limit 1000. Working
 fetchData(drivers_url);         //driver data, limit 1000. Working
 fetchData(constructors_url);    //driver data, limit 300. Working
+
+//print data as a string in the console, copy and place in data folder
+const printData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const dataString = JSON.stringify(data);
+    console.log(dataString);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("Finally block, runs regardsless. Fill in");
+  }
+};
+
+console.log("Printing Key Data:");
+printData(races_url);           //races data, limit 1000. Working
+printData(drivers_url);         //driver data, limit 1000. Working
+printData(constructors_url);    //driver data, limit 300. Working
