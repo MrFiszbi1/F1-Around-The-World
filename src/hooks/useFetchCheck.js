0985ae20b51ecde7f6f53e0e312052fd7bee4ae6
Gabriel Sampang <gsampang@cecs.pdx.edu>
2023-06-01 16:57:24 -0700

    Tested and polished useFetchCheck hook

--- src/hooks/useFetchCheck.js
+++ src/hooks/useFetchCheck.js
@@ -1,7 +1,7 @@
 import useFetchData from "./useFetchData"; //returns false if unable to fetch data.
 
 const useFetchCheck = async (url, file) => {
-  const data = await useFetchData(url);
+  let data = await useFetchData(url); //replace url w/ "badurl" to demo the backups
 
   if (data === false) {
     console.log(`API Error! Using backup file ${file} instead`);
