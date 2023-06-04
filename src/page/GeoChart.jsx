39ea9aeb5cd4c19ca697e9535c5e59e1ee32bdb5
MinhDucMa <77559009+MinhDucMa@users.noreply.github.com>
2023-06-04 15:04:13 -0700

    Update: Adding CardComponent to GeoChart page

--- src/page/GeoChart.jsx
+++ src/page/GeoChart.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import CardComponent from "../components/f2-sidebar";
 
 const GeoChart = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <h2>This is the Geo Chart page</h2>
+      <div style={{ alignSelf: "flex-end" }}>
+        <CardComponent region={"USA"} />
+      </div>
     </div>
   );
 };
